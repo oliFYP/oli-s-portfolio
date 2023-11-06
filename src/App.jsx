cd1d63aa513483c6c2ff8f4bdea211c3a0469905
oliFYP <k2026944@kingston.ac.uk>
2023-11-06 17:14:07 +0000

    add: new background, side panel toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import background from './videobg/4K_10.mp4';
 import logo from './img/OS.png';
 import logo1 from './img/insta.png';
@@ -7,6 +7,12 @@ import logo3 from './img/linkedIn.png';
 import menu from './img/menu.png';
 
 function App() {
+  const [isSidePanelOpen, setSidePanelOpen] = useState(false);
+
+  const toggleSidePanel = () => {
+    setSidePanelOpen(!isSidePanelOpen);
+  };
+
   return (
     <div className="absolute top-0 left-0 w-full h-full bg-center bg-cover">
       <video
@@ -26,31 +32,57 @@ function App() {
         <button className="bg-transparent font-bold text-white hidden sm:block mr-2">Projects</button>
         <button className="bg-transparent font-bold text-white hidden sm:block mr-2">Contact</button>
         <button className="bg-transparent font-bold text-white hidden sm:block mr-2">About</button>
-        <img src={menu} className="h-8 mr-2 sm:hidden" alt="Menu" />
+        <img
+          src={menu}
+          className="h-8 mr-2 sm:hidden cursor-pointer"
+          alt="Menu"
+          onClick={toggleSidePanel}
+        />
       </div>
 
       <div className="absolute top-0 left-0 m-4 flex">
         <img src={logo} className="h-8 mt-1 sm:h-9 sm:mt-2" alt="Logo" />
       </div>
 
-      <div className="absolute top-1/4 right-1/2 transform">
-        <div className="bg-white h-full w-1 absolute top-0 left-0"></div>
-        <h1 className="select-none text-white font-bold text-4xl md:text-4xl lg:text-6xl max-w-md pl-4">
-          Hi there!
-        </h1>
-        <p className="select-none text-white font-bold mt-3 text-lg lg:text-3xl lg:mt-10 max-w-md pl-4">
-          I'm Olivier, a computer science graduate
-        </p>
-        <button className="rounded-2xl ml-5 bg-white mt-10 w-40 h-10 font-bold text-black flex items-center justify-center transition-all">
-          Download CV
-        </button>
-        <div className="mt-3 flex">
-          <img src={logo1} className="h-8 mr-2 ml-8" alt="Logo 1" />
-          <img src={logo2} className="h-8 mr-2" alt="Logo 2" />
-          <img src={logo3} className="h-8" alt="Logo 3" />
-        </div>
-      </div>
+      <div className="absolute top-0 right-0">
+  {isSidePanelOpen && (
+    <div className="fixed top-0 right-0 h-full bg-black w-full transition-all flex flex-col items-center justify-center z-50">
+      <button className="bg-transparent font-bold text-white p-4 my-2 w-full">Home</button>
+      <button className="bg-transparent font-bold text-white p-4 my-2 w-full">Experience</button>
+      <button className="bg-transparent font-bold text-white p-4 my-2 w-full">Projects</button>
+      <button className="bg-transparent font-bold text-white p-4 my-2 w-full">Contact</button>
+      <button className="bg-transparent font-bold text-white p-4 my-2 w-full">About</button>
+      <button
+        className="bg-transparent rounded-full font-bold text-white p-4 my-2 w-auto"
+        onClick={toggleSidePanel}
+      >
+        X
+      </button>
     </div>
+  )}
+</div>
+
+
+<div className="absolute top-1/4 right-1/2 transform">
+  <div className="bg-white h-full w-1 absolute top-0 left-0" style={{ marginLeft: '10px' }}></div>
+  <div style={{ marginLeft: '10px' }}>
+    <h1 className="select-none text-white font-bold text-4xl md:text-4xl lg:text-6xl max-w-md pl-4">
+      Hi there!
+    </h1>
+    <p className="select-none text-white font-bold mt-3 text-lg lg:text-3xl lg:mt-10 max-w-md pl-4">
+      I'm Olivier, a computer science graduate
+    </p>
+    <button className="rounded-2xl ml-5 bg-white mt-10 w-40 h-10 font-bold text-black flex items-center justify-center transition-all">
+      Download CV
+    </button>
+    <div className="mt-3 flex">
+      <img src={logo1} className="h-8 mr-2 ml-8" alt="Logo 1" />
+      <img src={logo2} className="h-8 mr-2" alt= "Logo 2" />
+      <img src={logo3} className="h-8" alt="Logo 3" />
+    </div>
+  </div>
+  </div>
+</div>
   );
 }
 
