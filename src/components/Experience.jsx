d612bc0f67936a8c54cdbeaf8e9f04f918f98150
oliFYP <k2026944@kingston.ac.uk>
2023-11-09 15:54:37 +0000

    more changes regarding the experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -20,19 +20,19 @@ function Experience() {
   }, []);
 
   const iconSources = [icon1, icon2, icon3, icon4, icon5, icon6, icon7, icon8, icon9, icon10];
-  const titles = ['CSS', 'Dart', 'React', 'Tailwind','Python', 'Java', 'HTML', 'JavaScript', 'Firebase', 'MATLAB'];
+  const titles = ['CSS', 'Dart', 'React', 'Tailwind', 'Python', 'Java', 'HTML', 'JavaScript', 'Firebase', 'MATLAB'];
 
   return (
-    <div className="bg-black h-auto p-6">
+    <div className="bg-black h-auto w-auto p-6">
       <div className="max-w-5xl mx-auto">
-        <h1 data-aos="fade-down" className="text-2xl font-bold mb-4">
+        <h1 data-aos="fade-down" className="text-2xl font-bold mb-4 text-white text-center">
           Experience
         </h1>
-        <div className="flex flex-wrap  justify-center">
+        <div className="flex flex-wrap justify-center">
           {iconSources.map((icon, index) => (
-            <div key={index} className="  md:w-1/4 lg:w-1/5 p-2">
-              <div className="w-24 h-24 bg-gray-800 flex flex-col items-center justify-center rounded-lg">
-                <img src={icon} alt={`Icon ${index + 1}`} className="w-16 h-16 object-contain" data-aos={`fade-up-${index * 2}`}/>
+            <div key={index} className="w-1/2 md:w-1/4 lg:w-1/5 p-2">
+              <div className="w-32 h-32 md:w-40 md:h-40 lg:w-48 lg:h-48 bg-gray-800 flex flex-col items-center justify-center rounded-lg mx-auto">
+                <img src={icon} alt={`Icon ${index + 1}`} className="w-24 h-24 md:w-32 md:h-32 lg:w-40 lg:h-40 object-contain" data-aos={`fade-up-${index * 2}`} />
                 <div className="text-white font-bold text-center mt-1 mb-1">{titles[index]}</div>
               </div>
             </div>
