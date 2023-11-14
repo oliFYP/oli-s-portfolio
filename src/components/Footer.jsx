9003250a170434107ca51f50748f6ebed377cd15
oliFYP <k2026944@kingston.ac.uk>
2023-11-14 22:48:58 +0000

    Edited footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,21 +8,21 @@ const Footer = () => {
     <footer className="bg-gray-900  py-4 md:py-8">
     <div className="container mx-auto">
       <div className="flex flex-col md:flex-row justify-between items-center">
-        <p  className="text-sm md:text-base">&copy; 2023 Olivier Smith. All rights reserved.</p>
+        <p  className="text-sm md:text-base ml-0 lg:ml-5 ">&copy; 2023 Olivier Smith. All rights reserved.</p>
         <ul className="flex space-x-4 md:space-x-6">
           <li>
-            <a className="hover:text-gray-500 hover:cursor-pointer text-white text-sm md:text-base">
+            <a className="hover:text-gray-500 hover:cursor-pointer text-white font-bold text-sm md:text-base">
               Home
             </a>
           </li>
           <li>
-            <a className="hover:text-gray-500 hover:cursor-pointer text-white text-sm md:text-base" >
+            <a className="hover:text-gray-500 hover:cursor-pointer text-white font-bold text-sm md:text-base" >
               About Me
             </a>
           </li>
          
           <li>
-            <a className="hover:text-gray-500 hover:cursor-pointer text-white text-sm md:text-base" >
+            <a className="hover:text-gray-500 hover:cursor-pointer text-white  font-bold text-sm md:text-base mr-0 lg:mr-5" >
               Contact
             </a>
           </li>
