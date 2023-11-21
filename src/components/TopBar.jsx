4876f74888c98e3cb241bf2ecbd309e9efa7253e
oliFYP <k2026944@kingston.ac.uk>
2023-11-21 14:55:13 +0000

    removed outline focus

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -12,23 +12,23 @@ function Topbar({ isSidePanelOpen, toggleSidePanel,  scrollToExperience, scrollT
      
     <div className="absolute top-0 left-0 w-full h-full">
         <nav className="absolute top-0 right-0 m-4 flex">
-    <button className="bg-transparent font-bold text-white group btn btn-border-underline hidden md:block mr-2 relative">
+    <button className="focus:outline-none bg-transparent font-bold text-white group btn btn-border-underline hidden md:block mr-2 relative">
     Home
     <span className="absolute left-0 right-0 bottom-0 h-1 bg-white transform scale-x-0 transition-transform duration-300 group-hover:scale-x-100"></span>
   </button>
-  <button className="bg-transparent font-bold text-white group btn btn-border-underline hidden md:block mr-2 relative" onClick={scrollToExperience}>
+  <button className="focus:outline-none bg-transparent font-bold text-white group btn btn-border-underline hidden md:block mr-2 relative" onClick={scrollToExperience}>
     Experience & Projects
     <span className="absolute left-0 right-0 bottom-0 h-1 bg-white transform scale-x-0 transition-transform duration-300 group-hover:scale-x-100"></span>
   </button>
-  <button className="bg-transparent font-bold text-white group btn btn-border-underline hidden md:block mr-2 relative" onClick={scrollToEducation}>
+  <button className="focus:outline-none bg-transparent font-bold text-white group btn btn-border-underline hidden md:block mr-2 relative" onClick={scrollToEducation}>
     Education
     <span className="absolute left-0 right-0 bottom-0 h-1 bg-white transform scale-x-0 transition-transform duration-300 group-hover:scale-x-100"></span>
   </button>
-  <button className="bg-transparent font-bold text-white group btn btn-border-underline  hidden md:block mr-2 relative">
+  <button className="focus:outline-none bg-transparent font-bold text-white group btn btn-border-underline  hidden md:block mr-2 relative">
     Contact
     <span className="absolute left-0 right-0 bottom-0 h-1 bg-white transform scale-x-0 transition-transform duration-300 group-hover:scale-x-100"></span>
   </button>
-  <button className="bg-transparent font-bold text-white group btn btn-border-underline hidden md:block mr-2 relative">
+  <button className="focus:outline-none bg-transparent font-bold text-white group btn btn-border-underline hidden md:block mr-2 relative">
     About Me
     <span className="absolute left-0 right-0 bottom-0 h-1 bg-white transform scale-x-0 transition-transform duration-300 group-hover:scale-x-100"></span>
   </button>
