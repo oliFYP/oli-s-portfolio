895032388d13838bfb8d75d6a213a58564f6cfcc
oliFYP <k2026944@kingston.ac.uk>
2023-12-01 00:37:57 +0000

    Added email contact configuration

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,12 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 import background from "../videobg/4K_10.mp4";
 import { PacmanLoader } from "react-spinners";
+import emailjs from "@emailjs/browser";
 import Footer from "../components/Footer";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
 function Contact() {
-  const [isLoading, setLoading] = useState(true);
+  const [isLoading, setLoading] = useState(false);
+  const [isFormSubmitted, setFormSubmitted] = useState(false);
+  const form = useRef();
+  const navigate = useNavigate();
 
   useEffect(() => {
     AOS.init({
@@ -17,6 +22,36 @@ function Contact() {
   setTimeout(() => {
     setLoading(false);
   }, 3000);
+
+  const navigateToHome = () => {
+    navigate("/");
+  };
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    emailjs
+      .sendForm(
+        "service_nl8zo8b",
+        "template_ihj2onj",
+        form.current,
+        "XC6mSz84G-bDBAYYc"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message sent");
+          setFormSubmitted(true);
+          setLoading(false);
+        },
+        (error) => {
+          console.log(error.text);
+          setLoading(false);
+        }
+      );
+  };
 
   return (
     <div>
@@ -39,73 +74,85 @@ function Contact() {
             </video>
 
             <div className="absolute sm:top-1/2 sm:left-1/2 transform sm:-translate-x-1/2 sm:-translate-y-1/2 w-full sm:h-auto h-full sm:max-w-md bg-black p-8 rounded shadow-md">
-              <form className="w-full">
-                <h1
-                  data-aos="flip-right"
-                  className="text-4xl font-bold text-white mb-8"
-                >
-                  Contact Me!
-                </h1>
-                <div className="mb-4">
-                  <label
-                    htmlFor="name"
-                    className="block text-sm font-medium text-white"
+              {isFormSubmitted ? (
+                <div className="sm:mt-0 text-white font-bold text-center">
+                  <p>Message sent!</p>
+                  <button
+                    onClick={navigateToHome}
+                    className="bg-white text-black font-bold px-4 py-2 rounded-md mt-4"
                   >
-                    Your Name
-                  </label>
-                  <input
+                    Back to Home
+                  </button>
+                </div>
+              ) : (
+                <form ref={form} onSubmit={sendEmail} className="w-full">
+                  <h1
                     data-aos="flip-right"
-                    type="text"
-                    id="name"
-                    name="name"
-                    className="mt-1 p-2 w-full border rounded-md"
-                    placeholder="John Doe"
-                    required
-                  />
-                </div>
-                <div className="mb-4">
-                  <label
-                    htmlFor="email"
-                    className="block text-sm font-medium text-white"
+                    className="text-4xl font-bold text-white mb-8"
                   >
-                    Email
-                  </label>
-                  <input
-                    data-aos="flip-left"
-                    type="email"
-                    id="email"
-                    name="email"
-                    className="mt-1 p-2 w-full border rounded-md"
-                    placeholder="john@example.com"
-                    required
-                  />
-                </div>
-                <div className="mb-4">
-                  <label
-                    htmlFor="message"
-                    className="block text-sm font-medium text-white"
-                  >
-                    Message
-                  </label>
-                  <textarea
-                    data-aos="flip-right"
-                    id="message"
-                    name="message"
-                    rows="4"
-                    className="mt-1 p-2 w-full border rounded-md"
-                    placeholder="How can we help you?"
-                    required
-                  ></textarea>
-                </div>
+                    Contact Me!
+                  </h1>
+                  <div className="mb-4">
+                    <label
+                      htmlFor="name"
+                      className="block text-sm font-medium text-white"
+                    >
+                      Your Name
+                    </label>
+                    <input
+                      data-aos="flip-right"
+                      type="text"
+                      id="name"
+                      name="user_name"
+                      className="mt-1 p-2 w-full border rounded-md"
+                      placeholder="John Doe"
+                      required
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-medium text-white"
+                    >
+                      Email
+                    </label>
+                    <input
+                      data-aos="flip-left"
+                      type="email"
+                      id="email"
+                      name="user_email"
+                      className="mt-1 p-2 w-full border rounded-md"
+                      placeholder="john@example.com"
+                      required
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      htmlFor="message"
+                      className="block text-sm font-medium text-white"
+                    >
+                      Message
+                    </label>
+                    <textarea
+                      data-aos="flip-right"
+                      id="message"
+                      name="message"
+                      rows="4"
+                      className="mt-1 p-2 w-full border rounded-md"
+                      placeholder="How can we help you?"
+                      required
+                    ></textarea>
+                  </div>
 
-                <button
-                  data-aos="flip-right"
-                  type="submit"
-                  className="bg-gradient-to-r from-slate-500 to-slate-800 text-white px-4 py-2  rounded-md "
-                >
-                  Submit
-                </button>
-              </form>
+                  <button
+                    type="submit"
+                    value="send"
+                    className="bg-white font-bold text-black px-4 py-2 rounded-md"
+                  >
+                    Submit
+                  </button>
+                </form>
+              )}
             </div>
           </div>
           <Footer />
