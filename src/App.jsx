9b4a93b271f300c4fea834c4a2bc8af3bff4870d
oliFYP <k2026944@kingston.ac.uk>
2023-12-20 21:28:30 +0000

    Changed browser router to hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Contact from "./Pages/Contact";
 import Project from "./Pages/Projects";
@@ -7,7 +7,7 @@ import Project from "./Pages/Projects";
 function App() {
   return (
     <div>
-      <Router basename="/oli-s-portfolio">
+      <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
