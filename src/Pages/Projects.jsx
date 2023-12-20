308700b371f2925e50d20deb2eeef3500175c4fd
oliFYP <k2026944@kingston.ac.uk>
2023-12-20 18:50:03 +0000

    added margin to the project list

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import aofitness from "../Projects/ao-fitness.png";
 import kdevs from "../Projects/k-devs.png";
 import khf from "../Projects/khf.png";
 import vm from "../Projects/vm.png";
 import Footer from "../components/Footer";
+import { PacmanLoader } from "react-spinners";
 
 function Project() {
+  const [isLoading, setLoading] = useState(true);
+  setTimeout(() => {
+    setLoading(false);
+  }, 3000);
   const projects = [
     {
       id: 1,
@@ -36,27 +41,37 @@ function Project() {
   ];
 
   return (
-    <div className="relative">
-      <div className="flex flex-wrap justify-center bg-gradient-to-r from-slate-900 to-slate-900">
-        {projects.map((project) => (
-          <div
-            key={project.id}
-            className="max-w-xs sm:max-w-sm m-2 rounded-lg overflow-hidden shadow-lg bg-black text-white"
-          >
-            <div className="w-full">
-              <img
-                className="w-full h-auto"
-                src={project.image}
-                alt={project.title}
-              />
+    <div>
+      {isLoading ? (
+        <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-black">
+          <PacmanLoader color="#ffffff" />
+        </div>
+      ) : (
+        <>
+          <div className="relative">
+            <div className="flex flex-wrap justify-center bg-gradient-to-r from-slate-900 to-slate-900">
+              {projects.map((project) => (
+                <div
+                  key={project.id}
+                  className="max-w-xs sm:max-w-sm m-2 rounded-lg transition-transform transform overflow-hidden shadow-lg bg-black text-white hover:scale-110 m-[40px]"
+                >
+                  <div className="w-full">
+                    <img
+                      className="w-full h-auto"
+                      src={project.image}
+                      alt={project.title}
+                    />
+                  </div>
+                  <div className="w-full bg-gradient-to-r from-slate-500 to-slate-800 p-4">
+                    <h2 className="text-white font-bold">{project.title}</h2>
+                  </div>
+                </div>
+              ))}
             </div>
-            <div className="w-full bg-gradient-to-r from-slate-500 to-slate-800 p-4">
-              <h2 className="text-white font-bold">{project.title}</h2>
-            </div>
+            <Footer />
           </div>
-        ))}
-      </div>
-      <Footer />
+        </>
+      )}
     </div>
   );
 }
