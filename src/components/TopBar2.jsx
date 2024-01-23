077865170b4bdc17982a378b4ed9c7539844f3fb
oliFYP <k2026944@kingston.ac.uk>
2024-01-23 16:10:34 +0000

    Changed the margin on the right for hamburger icon

--- src/components/TopBar2.jsx
+++ src/components/TopBar2.jsx
@@ -77,7 +77,7 @@ function Topbar2({ isSidePanelOpen, toggleSidePanel }) {
           <span className="absolute left-0 right-0 bottom-0 h-1 bg-white transform scale-x-0 transition-transform duration-300 group-hover:scale-x-100"></span>
         </button>
 
-        <div className="md:hidden z-[801]">
+        <div className="md:hidden z-[801] mr-4">
           <Hamburger
             size={30}
             color="white"
