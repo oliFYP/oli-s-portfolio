0aeef715b870fd69fbb2264db383ea3a27bd5414
Olivier Smith <oliviersmith@Oliviers-MacBook-Pro.local>
2024-06-19 01:53:25 +0100

    sorting this fing out

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./Pages/Home";
 import Contact from "./Pages/Contact";
 import Project from "./Pages/Projects";
 import AOFitness from "./Pages/AOfitness";
-
+import KDevs from "./Pages/Kdevs";
 import KHF from "./Pages/Khf";
 import VM from "./Pages/Vm";
 import Fran from "./Pages/Fran";
@@ -27,7 +27,7 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           <Route path="/project" element={<Project />} />
           <Route path="/aofitness" element={<AOFitness />} />
-
+          <Route path="/kdevs" element={<KDevs />} />
           <Route path="/khf" element={<KHF />} />
           <Route path="/vm" element={<VM />} />
           <Route path="/fran" element={<Fran />} />
